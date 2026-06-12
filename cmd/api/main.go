1415b1a61b1d8327a8ed05923828cc6d52bff23b
Florian Renfer <florian.renfer@heyoptima.de>
2026-06-12 17:56:17 +0200

    feat(db): initialize pgxpool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,17 +4,24 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
 	"github.com/florian-renfer/property-service-tracking/internal/api/router"
 	"github.com/florian-renfer/property-service-tracking/internal/interface/rest"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
+	// Establish database connection
+	connect()
+
 	// Router dependencies
 	healthHandler := rest.NewHandler()
 
@@ -43,4 +50,31 @@ func main() {
 	if err := server.Shutdown(shutdownCtx); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func connect() {
+	databaseURL := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?pool_max_conns=10&pool_max_conn_lifetime=30m",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DB"),
+	)
+
+	dbpool, err := pgxpool.New(context.Background(), databaseURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	defer dbpool.Close()
+
+	var greeting string
+	err = dbpool.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(greeting)
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,10 @@ require github.com/go-chi/chi/v5 v5.3.0
 require (
 	github.com/ajg/form v1.5.1 // indirect
 	github.com/go-chi/render v1.0.3 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.10.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
 )
